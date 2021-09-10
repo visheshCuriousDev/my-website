56808730ee02a5275b14de2619075e826bb254f4
visheshCuriousDev <visheshvvvm@gmail.com>
2021-09-10 12:10:40 +0530

    Bug fix on the header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import {Header} from './components/header';
-import {MiniBytes} from './components/mini-bytes';
+import Header from './components/header';
+import MiniBytes from './components/mini-bytes';
 
 function App() {
   return (
