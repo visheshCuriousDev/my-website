71493a4c978a964dd503ac64493f2fd53cf1282f
visheshCuriousDev <visheshvvvm@gmail.com>
2021-09-12 17:33:00 +0530

    Added projects section and styling not done yet

---
+++ src/components/projects/project-card/index.js
@@ -0,0 +1,13 @@
+import './project-card.css';
+
+const ProjectCard = ({projectDetails = {}}) => {
+    return (
+        <div className="project-detail">
+            <div className="">
+                
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard;
